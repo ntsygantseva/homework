a64472ac8852b6027fb1d6481e8d91eba12892c6
Semyon Komyagin <komyagin.s@gmail.com>
2019-11-20 13:42:04 +0300

    Home work for Java AT

--- src/test/java/io/github/komyagin/strings/nonstart/StringNonStartTest.java
+++ src/test/java/io/github/komyagin/strings/nonstart/StringNonStartTest.java
@@ -8,31 +8,92 @@ public class StringNonStartTest {
 
     @Test
     public void shouldReturnRightConcatenationFirstCase() {
+        //GIVEN
         StringNonStart stringNonStart = new StringNonStart();
 
+        //WHEN
         String expected = "ellohere";
         String actual = stringNonStart.nonStart("Hello", "There");
 
+        //THEN
         assertEquals(expected, actual);
     }
 
     @Test
     public void shouldReturnRightConcatenationSecondCase() {
+        //GIVEN
         StringNonStart stringNonStart = new StringNonStart();
 
+        //WHEN
         String expected = "avaode";
         String actual = stringNonStart.nonStart("java", "code");
 
+        //THEN
         assertEquals(expected, actual);
     }
 
     @Test
     public void shouldReturnRightConcatenationThirdCase() {
+        //GIVEN
         StringNonStart stringNonStart = new StringNonStart();
 
+        //WHEN
         String expected = "hotlava";
         String actual = stringNonStart.nonStart("shotl", "java");
 
+        //THEN
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturnRightConcatenationFourthCase() {
+        //GIVEN
+        StringNonStart stringNonStart = new StringNonStart();
+
+        //WHEN
+        String expected = "by";
+        String actual = stringNonStart.nonStart("ab", "xy");
+
+        //THEN
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturnRightConcatenationFifthCase() {
+        //GIVEN
+        StringNonStart stringNonStart = new StringNonStart();
+
+        //WHEN
+        String expected = "b";
+        String actual = stringNonStart.nonStart("ab", "x");
+
+        //THEN
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturnRightConcatenationSixthCase() {
+        //GIVEN
+        StringNonStart stringNonStart = new StringNonStart();
+
+        //WHEN
+        String expected = "c";
+        String actual = stringNonStart.nonStart("x", "ac");
+
+        //THEN
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturnRightConcatenationSeventhCase() {
+        //GIVEN
+        StringNonStart stringNonStart = new StringNonStart();
+
+        //WHEN
+        String expected = "";
+        String actual = stringNonStart.nonStart("a", "x");
+
+        //THEN
         assertEquals(expected, actual);
     }
 
